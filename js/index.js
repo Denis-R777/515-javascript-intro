bf50da9c206a8c00e77749ac6a0812c7ed4acc36
Dmytro Romaniuk <mr.executive21@gmail.com>
2020-06-28 16:04:12 +0300

    added change

--- js/index.js
+++ js/index.js
@@ -17,10 +17,25 @@ alert("Your age is " + userAge);
 const nextAge = userAge + 1;
 alert("Next year you will be " + nextAge);
 
+//string
+//number
+//boolean   true; false; правда або неправда
+//undefined
+//null
+//object
+//ES2015:
+//symbol
+
 if (userAge >= 18) {
+  // !== не дорівнює    === дорівнює   && -енд     || - або
   alert("You are adult!");
 } else {
   alert("You are underaged");
+}
+
+function addOne(value) {
+  const result = value + 1;
+  return result;
 }
 
 function add(a, b) {
